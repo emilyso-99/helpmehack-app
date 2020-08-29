2bce131875caf4e743c226068768afd5b8d10662
emilyso-99 <emmy4short@gmail.com>
2020-08-29 19:38:42 -0400

    submitting for visual checking

--- helpmehack/lib/homepage.dart
+++ helpmehack/lib/homepage.dart
@@ -6,7 +6,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var scaffold = new Column(children: [
+    return new Scaffold(
+        body: SingleChildScrollView(
+            child: Column(children: [
       Stack(children: [
         Container(
             alignment: Alignment.center, color: Colors.deepPurple, height: 420),
@@ -25,25 +27,42 @@ class HomePage extends StatelessWidget {
                       decoration: TextDecoration.none)))
         ])
       ]),
-      ButtonBar(alignment: MainAxisAlignment.spaceAround, children: [
+      ButtonBar(alignment: MainAxisAlignment.spaceEvenly, children: [
         ButtonTheme(
           child: RaisedButton(
             onPressed: () {
               Navigator.of(context).pushNamed('/buildawebsite');
             },
             child:
-                const Text('Build a Website!', style: TextStyle(fontSize: 20)),
+                const Text('Build a Website!', style: TextStyle(fontSize: 40)),
           ),
-          minWidth: 200,
+          minWidth: 400,
+          height: 200,
         ),
-        const SizedBox(height: 30),
-        RaisedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed('/makeagame');
-            },
-            child: const Text('Build an app!', style: TextStyle(fontSize: 20)))
-      ])
-    ]);
-    return scaffold;
+        ButtonTheme(
+          child: RaisedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/makeagame');
+              },
+              child: const Text(
+                'Build an app!',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              )),
+          minWidth: 400,
+          height: 200,
+        )
+      ]),
+      Container(color: Colors.white, height: 200, alignment: Alignment.center),
+      Container(
+          child: Center(
+              child: Text(
+                  "Welcome to HelpMeHack, a place to help you start your first project, and find like minded coders!! ",
+                  style: TextStyle(
+                      fontFamily: "Comfortaa",
+                      fontSize: 30,
+                      color: Colors.deepPurple),
+                  textAlign: TextAlign.center)))
+    ])));
   }
 }
