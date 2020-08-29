e911a2063601827f0595b4397bbafeb0eed38d6d
emilyso-99 <emmy4short@gmail.com>
2020-08-29 16:42:01 -0400

    added scrolling for website

--- helpmehack/lib/website/website.dart
+++ helpmehack/lib/website/website.dart
@@ -13,7 +13,8 @@ class WebsitePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+      body: SingleChildScrollView(
+          child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
         AppBar(
           title: CenterHorizontal(Text("Building a Website!",
               style: TextStyle(
@@ -24,15 +25,26 @@ class WebsitePage extends StatelessWidget {
         ),
         Container(
             alignment: Alignment.center,
-            color: Colors.deepPurple,
-            child: Text("\nWebsites are fun!",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Comfortaa',
-                    fontSize: 36))),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.deepPurple, Colors.white])),
+            child: Text("\n\n\n")),
+        Container(
+            color: Colors.white,
+            child: Text("Websites are fun!", style: styler)),
+        Image(
+            image: AssetImage("lib/website/images/seven-grams-coffee.png"),
+            alignment: Alignment.center),
         ExpansionTile(
-          title: CenterHorizontal(Text("\nStep 1\n", style: styler)),
-          children: [Text("\nHTML\n")],
+          title: CenterHorizontal(
+              Text("\nStep 1:Making it Pretty \n", style: styler)),
+          children: [
+            Text(
+                "\n The key to a nice website is a strong HTML (Hyper Text Markup Language) framework." +
+                    " ")
+          ],
         ),
         ExpansionTile(
           title: CenterHorizontal(Text("\nStep 2\n", style: styler)),
@@ -40,7 +52,7 @@ class WebsitePage extends StatelessWidget {
         ),
         ExpansionTile(
             title: CenterHorizontal(Text("\nStep 3\n", style: styler)))
-      ]),
+      ])),
     );
   }
 }
